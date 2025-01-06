d52755cae8b58889ed3243f88655867c5e433b6b
Jiang <jiangliangiobit@gmail.com>
2025-01-06 17:49:30 +0800

    feat: 修改为vidwud上传地址

--- upload.js
+++ upload.js
@@ -1,27 +1,26 @@
-import axios from 'axios'
-import FormData from 'form-data'
-import fs from 'fs'
-import { Glob } from 'glob'
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
+import { Glob } from "glob";
 
-async function uploadFile (filepath) {
-    const data = new FormData()
-    data.append('api_key', 'Q^qH*X3FhmdB')
-    data.append('type', 'font')
-    data.append('file', fs.createReadStream(filepath))
+async function uploadFile(filepath) {
+  const data = new FormData();
+  data.append("api_key", "Q^qH*X3FhmdB");
+  data.append("type", "font");
+  data.append("file", fs.createReadStream(filepath));
 
-    return axios.request({
-        method: 'post',
-        url: 'https://api-test.vidnoz.com/ai/source/system-upload',
-        headers: {
-            ...data.getHeaders()
-        },
-        data: data
-    })
+  return axios.request({
+    method: "post",
+    url: "https://main-api-test.vidwud.com/apps/source/system-upload",
+    headers: {
+      ...data.getHeaders(),
+    },
+    data: data,
+  });
 }
 
-
-const g = new Glob(['fonts/*.{ttf,otf}', 'fonts-images/*.png'], { nodir: true })
+const g = new Glob(["fonts/*.{ttf,otf}", "fonts-images/*.png", "font-objects/*.eot"], { nodir: true });
 for (const filepath of g) {
-    console.log("🚀 ~ filepath:", filepath)
-    await uploadFile(filepath)
+  console.log("🚀 ~ filepath:", filepath);
+  await uploadFile(filepath);
 }
