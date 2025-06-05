edced0a127174e01b0d4ea80a25066971b253e64
Hanlin <onion7878@gmail.com>
2025-06-05 15:41:05 +0800

    feat: 修改上传参数及堆 eot 文件的上传

--- upload.js
+++ upload.js
@@ -3,25 +3,33 @@ import FormData from 'form-data'
 import fs from 'fs'
 import { Glob } from 'glob'
 
-async function uploadFile (filepath) {
+async function uploadFile (filepath, type = 'font') {
     const data = new FormData()
     data.append('api_key', 'Q^qH*X3FhmdB')
-    data.append('type', 'font')
+    data.append('type', type)
     data.append('file', fs.createReadStream(filepath))
 
     return axios.request({
         method: 'post',
         url: 'https://api-test.vidnoz.com/ai/source/system-upload',
         headers: {
+            'Authorization': 'Bearer phC1UjWsn-EP9XHwuXnbO8ahMxd4No05_2028178334',
             ...data.getHeaders()
         },
         data: data
     })
 }
 
-
-const g = new Glob(['fonts/*.{ttf,otf}', 'fonts-images/*.png'], { nodir: true })
-for (const filepath of g) {
+const gFonts = new Glob(['fonts/*.{ttf,otf}', 'fonts-images/*.png'], { nodir: true })
+for (const filepath of gFonts) {
     console.log("🚀 ~ filepath:", filepath)
-    await uploadFile(filepath)
+    const response = await uploadFile(filepath)
+    console.log("🚀 ~ response:", response.data)
+}
+
+const gEots = new Glob(['eots/*.eot'], { nodir: true })
+for (const filepath of gEots) {
+    console.log("🚀 ~ filepath:", filepath)
+    const response =  await uploadFile(filepath, 'eot')
+    console.log("🚀 ~ response:", response.data)
 }
